89f714b44016bc1b4a9feb1cf5b4ba240408a3ee
MarianoGomesOpitz <marianogomesopitz@gmail.com>
2022-05-23 21:40:01 -0300

    Ejercicio5 (V1)

---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+////Link válido
+int verif = 0;
+string link = "";
+Console.WriteLine("Ingrese un link válido:");
+do
+{
+    if (verif > 0)
+    {
+        if (Uri.IsWellFormedUriString(link, UriKind.Absolute))
+        {
+            Console.WriteLine("Link válido");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Link Inválido, ingrese de nuevo:");
+        }
+    }
+    link = Console.ReadLine();
+    verif++;
+} while (true);
+
+////Email válido
+verif = 0;
+string mail = "";
+Console.WriteLine("Ingrese un correo electrónico válido:");
+do
+{
+    if (verif > 0)
+    {
+        try
+        {
+            var addr = new System.Net.Mail.MailAddress(mail);
+            if (addr.Address == mail)
+            {
+                Console.WriteLine("Correo válido");
+                break;
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Correo inválido, ingrese de nuevo:");
+        }
+    }
+    mail = Console.ReadLine();
+    verif++;
+} while (true);
